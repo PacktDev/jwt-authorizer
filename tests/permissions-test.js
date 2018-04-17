52f6c59381d6a9e8ac6d896719dcbacd741589ea
Chris Key <chrisk@packtpub.com>
2018-04-17 16:40:46 +0100

    Converting to unit tests

--- tests/permissions-test.js
+++ tests/permissions-test.js
@@ -1,9 +1,15 @@
+/* eslint-env node, mocha */
+
 const JwtAuthorizer = require('../index');
+const chai = require('chai'); // eslint-disable-line
+const describe = chai.describe(); // eslint-disable-line
+const expect = chai.expect(); // eslint-disable-line
 
-const gPermsJSON = '{"auth":{"service":0,"access":1,"createRole":2,"assignPermToRole":4,"assignRoleToUser":8},"credits":{"service":1,"giveOne":1,"giveMany":2},"videoCaptions":{"service":2,"canUpload":1},"users":{"service":3,"view":1,"edit":2}}';
-const gPerms = JSON.parse(gPermsJSON);
+const gPermsJson = '{"auth":{"service":0,"access":1,"createRole":2,"assignPermToRole":4,"assignRoleToUser":8},"credits":{"service":1,"giveOne":1,"giveMany":2},"videoCaptions":{"service":2,"canUpload":1},"users":{"service":3,"view":1,"edit":2}}';
+const gPermsJsonBadService = '{"auth":{"service":"abc","access":1,"createRole":2,"assignPermToRole":4,"assignRoleToUser":8},"credits":{"service":1,"giveOne":1,"giveMany":2},"videoCaptions":{"service":2,"canUpload":1},"users":{"service":3,"view":1,"edit":2}}';
+const gPerms = JSON.parse(gPermsJson);
 
-const perm = new JwtAuthorizer.PermissionManager(gPermsJSON);
+const perm = new JwtAuthorizer.PermissionManager(gPermsJson);
 
 perm.addPermission(gPerms.auth.service, gPerms.auth.access);
 perm.addPermission(gPerms.auth.service, gPerms.auth.createRole);
@@ -13,6 +19,12 @@ perm.addPermission(gPerms.credits.service, gPerms.credits.giveOne);
 perm.addPermission(gPerms.credits.service, gPerms.credits.giveMany);
 perm.addPermission(gPerms.videoCaptions.service, gPerms.videoCaptions.canUpload);
 perm.addPermission(gPerms.users.service, gPerms.users.canView);
+
+describe('Bad JSON', () => {
+  it('Fail for service error', () => {
+    expect(new JwtAuthorizer.PermissionManager(gPermsJsonBadService).to.throw(new Error('Service [auth] has no numeric service index')));
+  });
+});
 
 console.log(perm.toString());
 console.log(perm.listPermissions());
